f032bd9d1a3701f97b51fa6935f3024b231ed0f6
Peter J. Keleher <keleher@cs.umd.edu>
2022-02-17 22:56:44 -0500

    auto

--- clean-example.py
+++ clean-example.py
@@ -8,8 +8,7 @@ conn = psycopg2.connect("dbname=flightsskewed user=vagrant")
 cur = conn.cursor()
 
 # cleanup
-cur.execute("delete from flewon where flightid='DL108' and flightdate='2015-09-25'")
-cur.execute("delete from customers where customerid='cust1000'")
-cur.execute("delete from customers where customerid='cust1001'")
-
+cur.execute("DELETE FROM flewon WHERE flightid='DL108' AND flightdate='2015-09-25'")
+cur.execute("DELETE FROM flewon WHERE customerid >= 'cust1000' AND LENGTH(customerid) = 8")
+cur.execute("DELETE FROM customers WHERE customerid >= 'cust1000' AND LENGTH(customerid) = 8")
 conn.commit()
